0b9050b8986bfbf084993173d6a6921a07c3dbf8
Victor Pascual <vikpascual@gmail.com>
2024-11-09 12:50:34 +0100

    Aspecto boton ligth-dark

--- frontend/public/js/theme.js
+++ frontend/public/js/theme.js
@@ -1,26 +1,30 @@
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 function setTheme(){
+    let light_theme_button = document.getElementById("ligth-theme-button")
+    let dark_theme_button  =  document.getElementById("dark-theme-button")
+
     document.documentElement.classList.toggle(
         'dark',
         localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
         )
+
+    if(localStorage.theme === 'dark' || (!('theme' in localStorage) && (window.matchMedia('(prefers-color-scheme: dark)').matches))){
+        dark_theme_button.className = "rounded dark:bg-white dark:text-black"
+        light_theme_button.className = "dark:text-white"
+    }else if(localStorage.theme === 'light'){
+        dark_theme_button.className = ""
+        light_theme_button.className = "rounded text-white bg-slate-950"
+    }
 }
 window.onload = function(){
     setTheme()
-    let light_theme_button = document.getElementById("ligth-theme-button")
-    let dark_theme_button  =  document.getElementById("dark-theme-button")
 
-    light_theme_button.addEventListener("click", function (e) {
+    document.getElementById("ligth-theme-button").addEventListener("click", function (e) {
         localStorage.theme = 'light'
-        this.className = "rounded text-white bg-slate-950"
-        dark_theme_button.className = ""
         setTheme()
     })
-
-    dark_theme_button.addEventListener("click", function (e) {
+    document.getElementById("dark-theme-button").addEventListener("click", function (e) {
         localStorage.theme = 'dark'
-        this.className = "rounded dark:bg-white dark:text-black"
-        light_theme_button.className = "dark:text-white"
         setTheme()
 
     })
