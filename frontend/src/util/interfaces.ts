66e494f2a9cff9c25b177b104a9ee0f5421afe8b
Victor Pascual <vikpascual@gmail.com>
2024-11-22 20:25:54 +0100

    Listo para build, nuevas interfaces

---
+++ frontend/src/util/interfaces.ts
@@ -0,0 +1,24 @@
+type TranslationData = {
+    [key: string]: string;
+};
+
+export interface Translations {
+    es: TranslationData;
+    en: TranslationData;
+}
+
+export interface Project {
+    image: string;
+    title: string  ; // Solo claves válidas de LocalizationData
+    description: string; // Solo claves válidas de LocalizationData
+    tags: string[];
+    year: number;
+}
+
+export type Projects = {
+    admin_oasis: Project;
+    q_learning_pong: Project;
+    space_simulator: Project;
+    planet_laser_locator: Project;
+    lichess_bot: Project;
+};
