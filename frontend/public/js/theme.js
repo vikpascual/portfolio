b5f8146d594d964e043bb3dbe42db35da2c990f3
Victor Pascual <vikpascual@gmail.com>
2025-01-27 11:17:06 +0100

    Visualizar imagenes
    Ahora se pueden ver las imagenes al hacer click

--- frontend/public/js/theme.js
+++ frontend/public/js/theme.js
@@ -1,8 +1,12 @@
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+// Al cargar la página o al cambiar de tema, es mejor agregar estilos en línea en el head para evitar el FOUC.
+// FOUC es que el contenido se muestra sin estilos y luego se aplica el estilo, lo que hace que el contenido se mueva. 
+// pero lo voy a dejar en un fichero aparte
 function setTheme(){
+    //pillamos botones de cambio de tema
     let light_theme_button = document.getElementById("ligth-theme-button")
     let dark_theme_button  =  document.getElementById("dark-theme-button")
 
+    //TODO: Si en el localstorage esta el tema claro hay que ponerlo
     document.documentElement.classList.toggle(
         'dark',
         localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
@@ -16,6 +20,9 @@ function setTheme(){
         light_theme_button.className = "p-2 rounded text-white bg-stone-800"
     }
 }
+//Funcion para cambiar el tema de la pagina, por default es dark, 
+//aunque se podria coger de las preferencias del ususario.
+//Pero los colores del tema claro no me convencen mucho, asi que lo dejo en dark por defecto
 window.onload = function(){
     localStorage.theme = 'dark'
     setTheme()
@@ -27,6 +34,36 @@ window.onload = function(){
     document.getElementById("dark-theme-button").addEventListener("click", function (e) {
         localStorage.theme = 'dark'
         setTheme()
-
     })
 }
+//Funcion para mostrar una imagen en pantalla completa, al darle click
+function showImage(image_url){
+    const imageview = document.getElementById("imageview-section");
+    let image = imageview?.getElementsByTagName("img")[0];
+    if (image){
+        image.src = image_url;
+    }
+    imageview?.classList.remove("hidden");
+}
+//Esconder la imagen al dar click en cualquier lugar del contenedor
+function hideImage(){
+    let imageview = document.getElementById("imageview-section");
+    imageview?.classList.add("hidden");
+}
+//Seteamos los eventos en las imagenes
+document.addEventListener("DOMContentLoaded", function(){
+    //esconder la imagen al dar click en cualquier lugar del contenedor
+    let imageview = document.getElementById("imageview-section");
+    imageview?.addEventListener("click", hideImage);
+    //como en astro no funciona el onclick ni aun con la directiva client:load, vamos a anyadirlo a todas las imagenes
+    let images = document.getElementsByTagName("img");
+    for(let i = 0; i < images.length; i++){
+        //si la imagen no es clickeable, no anyadir el evento
+        if(images[i].classList.contains("no-click")){
+            continue;
+        }
+        images[i].addEventListener("click", function(e){
+            showImage(e.target.src);
+        });
+    }
+});
